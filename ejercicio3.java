4577b30f0f64db4bcc8a62af62de63d33765aabe
RAESONKYUNG <139029008+RAESONKYUNG@users.noreply.github.com>
2023-09-02 20:37:05 -0500

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número (numA): ");
+        int numA = scanner.nextInt();
+        System.out.print("Ingrese el segundo número (numB): ");
+        int numB = scanner.nextInt();
+
+        boolean ambosPositivos = numA > 0 && numB > 0;
+
+        System.out.println("¿Ambos números son positivos? " + ambosPositivos);
+    }
+}
